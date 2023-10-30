a728b34d210896ffb4c4cb12a1a8dd07661a241c
plaze <banikakash131@gamil.com>
2023-10-30 21:03:45 +0530

    feat: add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { About, Testimonial, Header, Footer, Work, Skills } from "./container";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <About />
+      <Header />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
